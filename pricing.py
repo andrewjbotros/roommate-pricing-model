3db169abd620899fb33a0a9bfb0feb78c053e0b1
andrewjbotros <andrewjbotros@gmail.com>
2014-03-27 15:22:18 -0400

    monte carlo simulator for estimating probabilities of winning a single room given the opportunity to bid on multiple rooms

--- pricing.py
+++ pricing.py
@@ -1,16 +1,33 @@
 from random import randint
+from pylab import *
 
-roomA = [12,3,5,7,9,10,12,14,15,16]
-roomB = [12,3,4,6,7,8,10,11,12,13,14]
-roomC = [1,4,5,6,7,9,10,11,13,14]
-roomD = [12,5,7,8,10,11,13,14]
+roomA = [2,3,7,8,9,10,12,14,15,16,17]
+roomB = [2,3,4,5,8,9,10,11,13,14,16,17]
+roomC = [1,3,4,5,6,7,9,10,11,13,14,17]
+roomD = [1,5,6,7,10,11,13,17]
+unit = [roomA, roomB, roomC, roomD]
+students = 17
+trials = 1000
 result = []
-total = []
+check = []
+probabilities = []
 
-for _ in range(10):
-	randNumber = roomA[randint(0, len(roomA) - 1)]
-	print randNumber
-	result.append(randNumber)
-	print result
+def rollTheDice(room, check):
+	randNumber = randint(0, len(room))
+	if check.count(room[randNumber]) == 0:
+		check.append(room[randNumber])
+		result.append(room[randNumber])
+	else:
+		rollTheDice(room, check)
 
+for _ in range(trials):
+	for r in unit:
+		rollTheDice(r, check)
+	check = []
 
+for i in range(students):
+	probabilities.append(result.count(i+1))
+	print i + 1,
+	print float(result.count(i+1))/float(trials)
+
+print probabilities
